c7e27e6ac85c2950fdf28338e2e72d5b3bbdf540
MIDOWWEBCRETOR <midowwebcreater@gmail.com>
2025-09-07 13:13:21 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+// Simple lead/contact form demo
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Thank you for contacting Midow Web Creator! We will get back to you soon.");
+});
